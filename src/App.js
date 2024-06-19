ff732efa809bc7dbaa6c8a0ba3a0b4a78fe9628f
Zakhar Boldinov <developdr3am@gmail.com>
2024-06-19 20:47:47 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [posts, setPosts] = useState([
+        {id:1, title: "JavaScript", body: "JavaScript - язык программирования."},
+        {id:2, title: "JavaScript 2", body: "JavaScript - язык программирования."},
+        {id:3, title: "JavaScript 3", body: "JavaScript - язык программирования."}
+    ])
+
+    const [posts2, setPosts2] = useState([
+        {id:1, title: "Python", body: "Python - язык программирования."},
+        {id:2, title: "Python 2", body: "Python - язык программирования."},
+        {id:3, title: "Python 3", body: "Python - язык программирования."}
+    ])
+
+    return (
+
+        <div className='App'>
+            <PostList posts={posts} title = 'Посты про JS'/>
+            <PostList posts={posts2} title = 'Посты про Python'/>
+        </div>
+    );
+};
 
 export default App;
