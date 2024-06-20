055ce70d1377da9152c3d27e55d3782fc3ffd0e3
Bome9 <developdr3am@gmail.com>
2024-06-20 18:17:21 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 const App = () => {
 
     const [posts, setPosts] = useState([
-        {id:1, title: "JavaScript", body: "JavaScript - язык программирования."},
-        {id:2, title: "JavaScript 2", body: "JavaScript - язык программирования."},
-        {id:3, title: "JavaScript 3", body: "JavaScript - язык программирования."}
+        { id: 1, title: "JavaScript", body: "JavaScript - язык программирования." },
+        { id: 2, title: "JavaScript 2", body: "JavaScript - язык программирования." },
+        { id: 3, title: "JavaScript 3", body: "JavaScript - язык программирования." }
     ])
+    const [post, setPost] = useState({ title: '', body: '' })
 
-    const [posts2, setPosts2] = useState([
-        {id:1, title: "Python", body: "Python - язык программирования."},
-        {id:2, title: "Python 2", body: "Python - язык программирования."},
-        {id:3, title: "Python 3", body: "Python - язык программирования."}
-    ])
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+
+        setPosts([...posts, {...post, id: Date.now()}])
+        setPost({ title: '', body: '' })
+    }
 
     return (
 
         <div className='App'>
-            <PostList posts={posts} title = 'Посты про JS'/>
-            <PostList posts={posts2} title = 'Посты про Python'/>
+            <form>
+                {/* Управляемые компоненты */}
+                <MyInput
+                    value={post.title}
+                    onChange={e => setPost({...post, title: e.target.value})}
+                    placeholder='Название поста'
+                    type='text'
+                />
+                <MyInput
+                    value={post.body}
+                    onChange={e => setPost({...post, body: e.target.value})}
+                    placeholder='Описание поста'
+                    type='text'
+                />
+                <MyButton onClick={addNewPost} >Добавить</MyButton>
+            </form>
+            <PostList posts={posts} title='Посты про JS' />
         </div>
     );
 };
